e9a43aa72632fb23eb20b98ab832519f71394299
Leticia Araujo <leticiards.04@gmail.com>
2020-05-13 15:27:32 -0300

    Adding module of ViewChild

--- communication-app/src/app/parent-child/parent-child.component.ts
+++ communication-app/src/app/parent-child/parent-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-parent-child',
@@ -7,21 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentChildComponent implements OnInit {
 
+  @ViewChild("stopwatch2")
+  mytimer: TimerComponent;
+
+  @ViewChild("myP")
+  private myp: ElementRef; //Referência ao um elemento interno do HTML
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   start(){
-
+    this.mytimer.start();
   }
 
   stop(){
-
+    this.mytimer.stop();
   }
 
   clear(){
-    
+    this.mytimer.clear();
+  }
+
+  ngAfterViewInit(){
+    console.log(this.myp);
   }
 
 }
