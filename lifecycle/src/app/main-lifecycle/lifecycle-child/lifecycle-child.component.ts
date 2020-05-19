adfe62fbe972946fb286323e34c9e8b4d3d9d149
Leticia Araujo <leticiards.04@gmail.com>
2020-05-19 15:17:56 -0300

    Adding notes and Angular Hooks module

--- lifecycle/src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
+++ lifecycle/src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
@@ -1,20 +1,39 @@
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle-child',
   templateUrl: './lifecycle-child.component.html',
   styleUrls: ['./lifecycle-child.component.css']
 })
-export class LifecycleChildComponent implements OnInit {
+export class LifecycleChildComponent implements OnInit, OnDestroy, OnChanges {
   
   @Input() name: string;
   @Input() age: number;
   @Input() food: string;
   
-  constructor() { }
+  constructor() { 
+    console.log(this.name + " - constructor");
+  }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    console.log(this.name + " - ngOnInit");
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    console.log(this.name + " - ngOnChanges");
+  }
+
+  ngAfterContentInit(){
+    console.log(this.name + " - ngAfterContentInit");
+  }
+
+  ngAfterViewInit(){
+    console.log(this.name + " - ngAfterViewInit");
+  }
+
+  ngOnDestroy(){
+    console.log(this.name + " - ngOnDestroy");
   }
 
 }
