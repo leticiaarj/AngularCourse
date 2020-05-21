fe57aba5fc3e05b65e9ce710dd31d226deff74d8
Leticia Araujo <leticiards.04@gmail.com>
2020-05-21 12:18:41 -0300

    Adding edit module

--- lifecycle/src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
+++ lifecycle/src/app/main-lifecycle/lifecycle-child/lifecycle-child.component.ts
@@ -1,6 +1,11 @@
 
 import { Component, OnInit, Input, OnDestroy, OnChanges, SimpleChanges } from '@angular/core';
 
+export interface LifeCycleEvent{
+  id: number;
+  name: string;
+  color: string;
+}
 @Component({
   selector: 'app-lifecycle-child',
   templateUrl: './lifecycle-child.component.html',
@@ -11,29 +16,53 @@ export class LifecycleChildComponent implements OnInit, OnDestroy, OnChanges {
   @Input() name: string;
   @Input() age: number;
   @Input() food: string;
+
+  public events: LifeCycleEvent[] = [];
+  nextEventId: number= 0;
+
+  colors: string[] = ["accent", "warn", "primary"];
   
   constructor() { 
     console.log(this.name + " - constructor");
+    this.newEvent("constructor");
   }
 
   ngOnInit(){
     console.log(this.name + " - ngOnInit");
+    this.newEvent("ngOnInit");
   }
 
   ngOnChanges(changes: SimpleChanges){
     console.log(this.name + " - ngOnChanges");
+    this.newEvent("ngOnChanges");
   }
 
   ngAfterContentInit(){
     console.log(this.name + " - ngAfterContentInit");
+    this.newEvent("ngAfterContentInit");
   }
 
   ngAfterViewInit(){
     console.log(this.name + " - ngAfterViewInit");
+    this.newEvent("ngAfterViewInit");
   }
 
   ngOnDestroy(){
     console.log(this.name + " - ngOnDestroy");
+    this.newEvent("ngOnDestroy");
   }
 
+  newEvent(name: string){
+    let id = this.nextEventId++;
+    this.events.push({
+      id: id, 
+      color: this.colors[id%this.colors.length], 
+      name: name});
+    setTimeout(()=> {
+      let idx = this.events.findIndex((e) => e.id== id);
+      if (idx >= 0){
+        this.events.splice(idx, 1);
+      }
+    }, 3000 + this.events.length*2000);
+  }
 }
--- lifecycle/src/app/main-lifecycle/main-lifecycle.component.ts
+++ lifecycle/src/app/main-lifecycle/main-lifecycle.component.ts
@@ -14,7 +14,7 @@ export class MainLifecycleComponent implements OnInit {
   age: number;
   food: string;
 
-  editClient: Client= null;
+  editClient: number = -1;
 
   constructor() { }
 
@@ -22,16 +22,33 @@ export class MainLifecycleComponent implements OnInit {
   }
 
   save(){
-    if(this.editClient== null){
+    if(this.editClient== -1){
       this.clients.push({
         name: this.name,
         age: this.age,
         food: this.food
       });
     }
+    else{
+      this.clients[this.editClient].age= this.age;
+      this.clients[this.editClient].name= this.name;
+      this.clients[this.editClient].food= this.food;
+      this.editClient = -1;
+    }
     this.age = null;
     this.name = "";
     this.food = "";
+  }
+
+  remove(i: number){
+    this.clients.splice(i, 1);
+  }
+
+  edit(i: number){
+    this.age = this.clients[i].age;
+    this.name = this.clients[i].name;
+    this.food = this.clients[i].food;
+    this.editClient= i;
   }
 
 }
